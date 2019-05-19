9586f048abc0c5f4689eb202555ea664fc693ff4
Georgi Ivanov <pandoraast389@outlook.com>
2019-05-20 01:13:52 +0200

    Commit Correct (1)

---
+++ WebShop/Webshop/Webshop/Webshop/bestelbevestiging.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Webshop
+{
+    public partial class bestelbevestiging : System.Web.UI.Page
+    {
+        /*De tekst voor de bestelbevestiging hebben we vastgecodeerd in bestelbevestiging.aspx
+         Hier halen we de Sessie variabelen voor de ordernummer en de totaal prijs.*/
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblOrderNr.Text = Convert.ToInt32(Session["ordernr"]).ToString();
+            lblTotaal.Text = "€ " + Convert.ToDouble(Session["totaal"]);
+
+        }
+
+        /* Bij het klikken van deze knop wordt de gebruiker terug naar de catalogus doorverwezen.*/
+
+        protected void btnTerugNaarCath_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("default.aspx");
+        }
+    }
+}
